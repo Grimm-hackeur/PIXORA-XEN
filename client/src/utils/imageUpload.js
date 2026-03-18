b18c560979de27f829a468115f14989fdba6cbfb
ZEPHYR_DEV <klivensjeanjacques@gmail.com>
2026-03-17 19:26:33 -0500

    Update image upload validation and configuration

--- client/src/utils/imageUpload.js
+++ client/src/utils/imageUpload.js
@@ -1,44 +1,36 @@
 export const checkImage = (file) => {
-    let err = "";
-    if(!file){
-        return err = "File does not exist.";
-    }
-//?1 mb
-    if(file.size > 1024 * 1024){
-         return (err = "File size must be less than 1 Mb.");
-    }
-
-    if (file.type !== 'image/jpeg' && file.type !== 'image/png') {
-      return (err = "Image must be jpeg or png.");
-    }
-
-    return err;
-}
+  let err = "";
+  if (!file) return (err = "File does not exist.");
+  if (file.size > 1024 * 1024 * 5) return (err = "File size must be less than 5 Mb.");
+  if (file.type !== "image/jpeg" && file.type !== "image/png") return (err = "Image must be jpeg or png.");
+  return err;
+};
 
 export const imageUpload = async (images) => {
-    let imgArr = [];
-    for(const item of images){
-        const formData = new FormData();
+  const CLOUDINARY_UPLOAD_PRESET = process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET;
+  const CLOUDINARY_CLOUD_NAME = process.env.REACT_APP_CLOUDINARY_CLOUD_NAME;
+  const CLOUDINARY_URL = `https://api.cloudinary.com/v1_1/${CLOUDINARY_CLOUD_NAME}/upload`;
 
-        if(item.camera){
-            formData.append("file", item.camera);
-        }else{
-            formData.append("file", item);  
-        }
+  let imgArr = [];
+  for (const item of images) {
+    const formData = new FormData();
 
-        
-        formData.append("upload_preset", "ADD VALUE HERE");
-        formData.append("cloud_name", "ADD VALUE HERE");
-
-        const res = await fetch("ADD CLOUDINARY IMAGE UPLOAD LINK HERE", {
-            method: "POST",
-            body: formData
-        })
-
-        const data = await res.json();
-        imgArr.push({ public_id: data.public_id, url: data.secure_url });
-        
-      
+    if (item.camera) {
+      formData.append("file", item.camera);
+    } else {
+      formData.append("file", item);
     }
-    return imgArr;
-}
+
+    formData.append("upload_preset", CLOUDINARY_UPLOAD_PRESET);
+    formData.append("cloud_name", CLOUDINARY_CLOUD_NAME);
+
+    const res = await fetch(CLOUDINARY_URL, {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await res.json();
+    imgArr.push({ public_id: data.public_id, url: data.secure_url });
+  }
+  return imgArr;
+};
