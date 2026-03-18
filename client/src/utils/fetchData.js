f4f1778ec94f7c7c2a4df069528112af86211017
ZEPHYR_DEV <klivensjeanjacques@gmail.com>
2026-03-17 19:23:48 -0500

    Refactor fetchData.js to use API_URL and withCredentials
    Updated API calls to use the base API URL from environment variables and added withCredentials option.

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -1,37 +1,43 @@
 import axios from 'axios';
 
-export const getDataAPI = async (url,token) => {
-    const res = await axios.get(`/api/${url}`,{
-        headers: { Authorization: token}
-    });
-    return res;
-}
+const API_URL = process.env.REACT_APP_API_URL || '';
+
+export const getDataAPI = async (url, token) => {
+  const res = await axios.get(`${API_URL}/api/${url}`, {
+    headers: { Authorization: token },
+    withCredentials: true,
+  });
+  return res;
+};
 
 export const postDataAPI = async (url, post, token) => {
-  const res = await axios.post(`/api/${url}`, post, {
+  const res = await axios.post(`${API_URL}/api/${url}`, post, {
     headers: { Authorization: token },
+    withCredentials: true,
   });
   return res;
 };
 
 export const putDataAPI = async (url, post, token) => {
-  const res = await axios.put(`/api/${url}`, post, {
+  const res = await axios.put(`${API_URL}/api/${url}`, post, {
     headers: { Authorization: token },
+    withCredentials: true,
   });
   return res;
 };
 
 export const patchDataAPI = async (url, post, token) => {
-  const res = await axios.patch(`/api/${url}`, post, {
+  const res = await axios.patch(`${API_URL}/api/${url}`, post, {
     headers: { Authorization: token },
+    withCredentials: true,
   });
   return res;
 };
 
-
 export const deleteDataAPI = async (url, token) => {
-  const res = await axios.delete(`/api/${url}`, {
+  const res = await axios.delete(`${API_URL}/api/${url}`, {
     headers: { Authorization: token },
+    withCredentials: true,
   });
   return res;
 };
